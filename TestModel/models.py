a55839d75a75adfdc0ec756c549637a81c34a5ff
闻武(Vim) <wenwu_1991@163.com>
2018-03-28 18:44:51 +0800

    modify

--- TestModel/models.py
+++ TestModel/models.py
@@ -1,3 +1,4 @@
+# -*- coding: UTF-8 -*-
 from django.db import models
 
 # Create your models here.
@@ -6,13 +7,35 @@ class Requirement(models.Model):
 	startover_no = models.CharField(max_length=500)
 	pro_name = models.CharField(max_length=500)
 	sys_name = models.CharField(max_length=500)
+	sys_id = models.IntegerField()
 	thr_sys_test = models.FloatField()
 	thr_check_test = models.FloatField()
 	thr_mock_test = models.FloatField()
 
-# 各系统版本记录	
+# 各系统小版本 版本记录	
 class ReleaseNote(models.Model):
 	requirement_id = models.IntegerField()
 	sys_name = models.CharField(max_length=500)
+	sys_id = models.IntegerField()
 	create_time = models.DateField()
 	update_time = models.DateField(null=True)
+
+
+class staff(models.Model):
+	name = models.CharField(max_length=50)
+	status = models.IntegerField()
+
+class systemVersionPlan(models.Model):
+	sys_id = models.IntegerField()
+	sys_name = models.CharField(max_length=500)
+	stage = models.CharField(max_length=10)
+	plan_start_date = create_time = models.DateField()
+	actual_start_date = create_time = models.DateField(null=True)
+	plan_end_date = create_time = models.DateField()
+	actual_end_date = create_time = models.DateField(null=True)
+	
+	
+
+class assignVersion(models.Model):
+	staff_id = models.IntegerField()
+
